0c2560a57ad75b4b8120060050d6d3ac2591ec28
Chrisxavix <chrischillogallid64@outlook.com>
2024-06-25 23:32:19 -0500

    FormApp - switches validators

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './switches-page.component.css'
 })
 export class SwitchesPageComponent {
+
+  constructor(
+    private fb: FormBuilder,
+  ) {}
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ["M", Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  })
+
+  onSave() {
+    console.log("xD: ", this.myForm.controls);
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log("Save: ", this.myForm.value);
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
 
 }
