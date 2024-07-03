85868ee54f952629367a179e9b1ea7826bd30ac2
Chrisxavix <chrischillogallid64@outlook.com>
2024-07-02 23:28:42 -0500

    FormApp - validate sincrono forms

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+
+  }
+
+  /* si es síncrono se manda en el mismo array:
+    username: ["", [Validators.required, cantBeStrider]],
+  */
+  public myForm: FormGroup = this.fb.group({
+    name: ["", [Validators.required]],
+    email: ["", [Validators.required]],
+    username: ["", [Validators.required, cantBeStrider]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+    password2: ["", [Validators.required]],
+  })
+
+  onSubmit(): void {
+    if(this.myForm.invalid) {
+      console.log("Form inválido");
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log("Form inválido", this.myForm.value);
+  }
 
 }
